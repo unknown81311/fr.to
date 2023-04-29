777244673636f16a838d0d27e5d810dec1f36b4d
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-04-28 23:14:48 -0400

    Set build configuration; add gitkeep for static directory.

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ git config user.name "GoCD Automation"
 git config user.email "no-reply@dotglitch.dev"
 
 npm version patch
-version=$(npm version --json | jq '.dankpods' | tr -d '"')
+version=$(npm version --json | jq '."fr.to"' | tr -d '"')
 
 # Run the build
 npm i
 npm run build
-docker build . -t harbor.dotglitch.dev/library/dankpods:$version
+docker build . -t harbor.dotglitch.dev/library/fr-to:$version
 
 # Once built, push the new build number
 git add package.json
@@ -16,9 +16,7 @@ git commit -m "Bump version"
 git push
 
 # Push the new docker image
-docker push harbor.dotglitch.dev/library/dankpods:$version
-
-export CI_SUBJECT_VERSION=
+docker push harbor.dotglitch.dev/library/fr-to:$version
 
 # Inject the version number into the deployment files
 sed -i -e "s/:latest/:$version/g" k3s.yml
