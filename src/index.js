d9b37962b60037f8641781262af70cf1ea747099
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-04-29 22:55:01 -0400

    Set absolute dir for static

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 // Serve files in static
-app.use(express.static("../static"));
+app.use(express.static(__dirname + "/../static"));
 
 app.get('/', (req, res) => {
   res.send('Hello Express app!')
