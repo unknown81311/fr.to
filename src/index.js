997d5c74e06cdb1db151d4d236ac06168b24e46a
unknown81311 <43104779+unknown81311@users.noreply.github.com>
2023-04-30 12:27:29 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 // Serve files in static
 app.use(express.static(path.join(__dirname, "..","static")));
 app.get('/api/ip',(req,res)=>{
-  res.send(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+  res.send(req.headers['x-forwarded-for'] +"|"+ req.connection.remoteAddress);
 })
 app.get('/', (req, res) => {
   res.send('Hello Express app!')
