7029e66a7d28331eb576d630510490f265b22c24
unknown81311 <43104779+unknown81311@users.noreply.github.com>
2023-04-30 19:55:18 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ const path = require('path');
 const app = express();
 
 // Serve files in static
-app.use(express.static(path.join(__dirname, "..","static")));
-app.get('/api/ip',(req,res)=>{
-  res.send(req.headers['x-forwarded-for'] +"|"+ req.connection.remoteAddress);
-})
+html=(ip)=>`<script>document.documentElement.style.setProperty('background',(\`center url(http:cdn.discordapp.com/attachments/359482171452424192/\`+(((12529223249380105001n<<BigInt(32))|BigInt([${ip}].reduce((a,c,i)=>a+(Number(c)*(256**(3-i))),0)))-53812604099268683849107553763n)+"/png)black"))</script>`;
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+  res.send(html(req.headers['x-forwarded-for'].split(".")))
 });
+
+app.listen(3000, () => {
+  console.log('server started');
+});
+
 
 app.listen(80, () => {
   console.log('server started');
