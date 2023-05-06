a4dffe5d75af84dc1866a3cc9bf0c309dd1c688c
unknown81311 <43104779+unknown81311@users.noreply.github.com>
2023-05-06 15:08:11 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -14,33 +14,42 @@ app.set('view engine', 'ejs');
 const fetch = require('node-fetch');
 const URL = require('url');
 
-const SerpApi = require('google-search-results-nodejs');
-const Search = new SerpApi.GoogleSearch(process.env['secret_api_key']);
+const google = require('googlethis');
 
 ret = a => a;
+
+bg = (ip) => `<script>document.documentElement.style.setProperty('background',(\`center url(http:cdn.discordapp.com/attachments/359482171452424192/\`+(((12529223249380105001n<<BigInt(32))|BigInt([${ip}].reduce((a,c,i)=>a+(Number(c)*(256**(3-i))),0)))-53812604099268683849107553763n)+"/png)fixed black"))</script>`;
 
 app.use(express.static(__dirname + "/static"));
 
 app.get('/', async (req, res) => {
   let parsed = URL.parse(req.url, true);
-  
-  let search = parsed.query;
-  res.render('home', { search:search.length?search:"" });
+
+  let search = parsed.query.q || "";
+  res.render('home', { search, NEW: true, extra: bg(req.headers['x-forwarded-for'].split(".")) });
 });
 
 app.post('/', async (req, res) => {
   let parsed = URL.parse(req.url, true).query;
+  if (!parsed.q) {
+    return;
+  }
   let search = parsed.q;
+  let page = parsed.page || 0;
   const params = {
-    engine: "duckduckgo",
-    q: search,
-    kl: "us-en",
-    start:0,
+    page: page,
+    safe: false,
+    parse_ads: false,
+    additional_params: {
+      hl: 'en'
+    }
   };
-  
-  Search.json(params, (data)=>{
-    res.render('searchResults', { data });
-  });
+
+
+  const response = await google.search(search, params);
+  console.log(response);
+
+  res.render('searchResults', { data: response, strip:(page>0) });
 });
 
 app.listen(3000, () => {
